2f219f338af5e71ec88e849a6d39d64c0948aba7
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-01-28 18:46:51 +0700

    refactor: remove Inconsolata and let Tailwind handle it by itself

--- src/lib/template.ts
+++ src/lib/template.ts
@@ -54,7 +54,6 @@ export async function generateContent(
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400&display=swap" rel="stylesheet">
       ${font}
       <script src="https://cdn.tailwindcss.com"></script>
       <script>
@@ -62,16 +61,10 @@ export async function generateContent(
           theme: {
             fontFamily: {
               sans: ['${content.fontFamily || 'Inter'}'],
-              mono: ['Inconsolata'],
             },
           },
         };
       </script>
-      <style>
-        code {
-          @apply font-mono whitespace-pre-wrap;
-        }
-      </style>
     </head>
 
     <body>
