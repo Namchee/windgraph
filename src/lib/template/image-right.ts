0e9c24e0c231b6a55f273f979a06fa0fc8252b7b
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-08-08 21:11:22 +0700

    feat: create image-left and image-right template

---
+++ src/lib/template/image-right.ts
@@ -0,0 +1,25 @@
+export const IMAGE_RIGHT = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {fonts}
+    <script src="https://cdn.tailwindcss.com"></script>
+    {scripts}
+  </head>
+
+  <body class="{container}">
+    <div class="flex flex-col justify-between">
+      <div>
+        {title}
+        {subtitle}
+      </div>
+
+      {footer}
+    </div>
+
+    <div>
+      {image}
+    </div>
+  </body>
+</html>`;
